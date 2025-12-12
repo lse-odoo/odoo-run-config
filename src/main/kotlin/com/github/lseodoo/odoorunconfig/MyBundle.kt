866a9473917ba53aae4489c4534f473eed13e4aa
GitHub Action <action@github.com>
2025-12-12 21:39:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/lseodoo/odoorunconfig/MyBundle.kt
+++ src/main/kotlin/com/github/lseodoo/odoorunconfig/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lseodoo.odoorunconfig
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lseodoo/odoorunconfig/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/lseodoo/odoorunconfig/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.lseodoo.odoorunconfig.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
