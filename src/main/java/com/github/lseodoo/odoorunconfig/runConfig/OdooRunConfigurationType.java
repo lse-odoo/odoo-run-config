356969f631f5d4216b7bcae10d3b4a53574a2e34
lse-odoo <lse@odoo.com>
2026-01-07 15:11:43 +0100

    Replace default console icon with custom Odoo logo in run configuration type

--- src/main/java/com/github/lseodoo/odoorunconfig/runConfig/OdooRunConfigurationType.java
+++ src/main/java/com/github/lseodoo/odoorunconfig/runConfig/OdooRunConfigurationType.java
@@ -1,15 +1,19 @@
 package com.github.lseodoo.odoorunconfig.runConfig;
 
 import com.intellij.execution.configurations.ConfigurationTypeBase;
-import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.util.NotNullLazyValue;
 
 final class OdooRunConfigurationType extends ConfigurationTypeBase {
     static final String ID = "OdooRunConfiguration";
 
     OdooRunConfigurationType() {
-        super(ID, "Odoo", "Odoo run configuration type",
-                NotNullLazyValue.createValue(() -> AllIcons.Nodes.Console));
+        super(
+                ID,
+                "Odoo",
+                "Odoo run configuration type",
+                NotNullLazyValue.createValue(() -> IconLoader.getIcon("/icons/odoo_o_logo.svg", OdooRunConfigurationType.class))
+        );
         addFactory(new OdooConfigurationFactory(this));
     }
 }
