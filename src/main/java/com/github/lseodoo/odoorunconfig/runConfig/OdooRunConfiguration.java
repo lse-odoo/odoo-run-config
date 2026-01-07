3d94e793cad3649ef506496f8026189188ee49cf
lse-odoo <lse@odoo.com>
2026-01-07 18:14:58 +0100

    [ADD] custom field support and editor for Odoo run configuration
    Added a custom "Odoo Custom Field" with persistence support via `writeExternal`/`readExternal`. Introduced `OdooConfigurationFragmentEditor` to manage custom field edits in the UI.

--- src/main/java/com/github/lseodoo/odoorunconfig/runConfig/OdooRunConfiguration.java
+++ src/main/java/com/github/lseodoo/odoorunconfig/runConfig/OdooRunConfiguration.java
@@ -1,12 +1,17 @@
 package com.github.lseodoo.odoorunconfig.runConfig;
 
+import com.github.lseodoo.odoorunconfig.runConig.OdooConfigurationFragmentEditor;
 import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.JDOMExternalizerUtil;
 import com.jetbrains.python.run.PythonRunConfiguration;
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
 
 public class OdooRunConfiguration extends PythonRunConfiguration {
 
-//    private OdooRunConfigurationOptions odooOptions = new OdooRunConfigurationOptions();
+    private String myCustomField = ""; // Initialize to avoid nulls
 
     // The factory creates the configuration with a name.
     // We must call the parent's 2-argument constructor and then set the name.
@@ -15,28 +20,31 @@ public class OdooRunConfiguration extends PythonRunConfiguration {
         setName(name);
     }
 
-//    @Override
-//    public void writeExternal(@NotNull Element element) {
-//        super.writeExternal(element);
-//        Element odooOptionsElement = new Element("odoo-options");
-//        XmlSerializer.serializeInto(this.odooOptions, odooOptionsElement);
-//        element.addContent(odooOptionsElement);
-//    }
-//
-//    @Override
-//    public void readExternal(@NotNull Element element) {
-//        super.readExternal(element);
-//        Element odooOptionsElement = element.getChild("odoo-options");
-//        if (odooOptionsElement != null) {
-//            XmlSerializer.deserializeInto(this.odooOptions, odooOptionsElement);
-//        }
-//    }
+    public String getMyCustomField() {
+        return myCustomField;
+    }
 
-//    @Override
-//    protected @NotNull SettingsEditor<PythonRunConfiguration> createConfigurationEditor() {
-//        SettingsEditorGroup<PythonRunConfiguration> group = new SettingsEditorGroup<>();
-//        group.addEditor("Configuration", new PythonRunConfigurationEditor(this));
-//        group.addEditor("Odoo", new OdooSettingsEditor());
-//        return group;
-//    }
+    public void setMyCustomField(String myCustomField) {
+        this.myCustomField = myCustomField;
+    }
+
+    @Override
+    public void readExternal(@NotNull Element element) {
+        super.readExternal(element);
+        // Load our custom field
+        setMyCustomField(JDOMExternalizerUtil.readField(element, "MY_CUSTOM_FIELD", ""));
+    }
+
+    @Override
+    public void writeExternal(@NotNull Element element) {
+        super.writeExternal(element);
+        // Save our custom field
+        JDOMExternalizerUtil.writeField(element, "MY_CUSTOM_FIELD", getMyCustomField());
+    }
+
+    @NotNull
+    @Override
+    protected SettingsEditor<PythonRunConfiguration> createConfigurationEditor() {
+        return new OdooConfigurationFragmentEditor(this);
+    }
 }
---
+++ src/main/kotlin/com/github/lseodoo/odoorunconfig/runConig/OdooConfigurationFragmentEditor.kt
@@ -0,0 +1,35 @@
+package com.github.lseodoo.odoorunconfig.runConig
+
+import com.github.lseodoo.odoorunconfig.runConfig.OdooRunConfiguration
+import com.intellij.execution.ui.SettingsEditorFragment
+import com.intellij.openapi.ui.LabeledComponent
+import com.intellij.ui.components.JBTextField
+import com.jetbrains.python.run.PythonRunConfiguration
+import com.jetbrains.python.run.configuration.AbstractPythonConfigurationFragmentedEditor
+
+// Note: we can't override from PythonConfigurationFragmentedEditor as "This type is final, so it cannot be extended."
+class OdooConfigurationFragmentEditor(odooRunConfiguration: OdooRunConfiguration)
+    : AbstractPythonConfigurationFragmentedEditor<PythonRunConfiguration>(odooRunConfiguration) {
+
+    override fun customizeFragments(fragments: MutableList<SettingsEditorFragment<PythonRunConfiguration, *>>) {
+//        super.customizeFragments(fragments) // Can't call as parent is abstrat
+
+        // Add your custom fragment
+        fragments.add(createOdooCustomFieldFragment())
+    }
+
+    private fun createOdooCustomFieldFragment(): SettingsEditorFragment<PythonRunConfiguration, LabeledComponent<JBTextField>> {
+        val textField = JBTextField()
+        val component = LabeledComponent.create(textField, "Odoo Custom Field:")
+
+        return SettingsEditorFragment(
+            "odoo.my.custom.field",
+            "Odoo Settings",
+            "Odoo",
+            component,
+            { config, _ -> (config as? OdooRunConfiguration)?.let { textField.text = it.myCustomField } },
+            { config, _ -> (config as? OdooRunConfiguration)?.let { it.myCustomField = textField.text } },
+            { true }
+        )
+    }
+}
