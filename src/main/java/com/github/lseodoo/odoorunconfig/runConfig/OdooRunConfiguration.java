75c16e37c87a969fc7d1ee1660e56e546bebe5e1
lse-odoo <lse@odoo.com>
2026-01-09 19:13:13 +0100

    [ADD] Use an odoo-bin field to be used as python script parameter
    - Introduced persistence for `odoo-bin` file path using `writeExternal`/`readExternal`.
    - Replaced "Odoo Custom Field" with a dedicated `odoo-bin` file path field and validation in the configuration.
    - Updated UI editor to include a file chooser for selecting the `odoo-bin` path.

--- src/main/java/com/github/lseodoo/odoorunconfig/runConfig/OdooRunConfiguration.java
+++ src/main/java/com/github/lseodoo/odoorunconfig/runConfig/OdooRunConfiguration.java
@@ -2,6 +2,7 @@ package com.github.lseodoo.odoorunconfig.runConfig;
 
 import com.github.lseodoo.odoorunconfig.runConig.OdooConfigurationFragmentEditor;
 import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.RuntimeConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.JDOMExternalizerUtil;
@@ -11,7 +12,9 @@ import org.jetbrains.annotations.NotNull;
 
 public class OdooRunConfiguration extends PythonRunConfiguration {
 
-    private String myCustomField = ""; // Initialize to avoid nulls
+    public static final String ODOO_BIN_PATH_NAME = "ODOO_BIN_PATH_NAME";
+
+    private String odooBinFilePath;
 
     // The factory creates the configuration with a name.
     // We must call the parent's 2-argument constructor and then set the name.
@@ -20,26 +23,37 @@ public class OdooRunConfiguration extends PythonRunConfiguration {
         setName(name);
     }
 
-    public String getMyCustomField() {
-        return myCustomField;
+    public String getOdooBinFilePath() {
+        return odooBinFilePath;
     }
 
-    public void setMyCustomField(String myCustomField) {
-        this.myCustomField = myCustomField;
+    public void setOdooBinFilePath(String myCustomField) {
+        this.odooBinFilePath = myCustomField;
+        // The odoo-bin path is the file to run as a script (in pycharm run config context)
+        setScriptName(odooBinFilePath);
+    }
+
+    @Override
+    public void checkConfiguration() throws RuntimeConfigurationException {
+        super.checkConfiguration();
+
+        if (!odooBinFilePath.endsWith("odoo-bin")) {
+            throw new RuntimeConfigurationException("The odoo-bin file path must end with 'odoo-bin'");
+        }
     }
 
     @Override
     public void readExternal(@NotNull Element element) {
         super.readExternal(element);
         // Load our custom field
-        setMyCustomField(JDOMExternalizerUtil.readField(element, "MY_CUSTOM_FIELD", ""));
+        odooBinFilePath = JDOMExternalizerUtil.readField(element, ODOO_BIN_PATH_NAME);
     }
 
     @Override
     public void writeExternal(@NotNull Element element) {
         super.writeExternal(element);
         // Save our custom field
-        JDOMExternalizerUtil.writeField(element, "MY_CUSTOM_FIELD", getMyCustomField());
+        JDOMExternalizerUtil.writeField(element, ODOO_BIN_PATH_NAME, odooBinFilePath);
     }
 
     @NotNull
--- src/main/kotlin/com/github/lseodoo/odoorunconfig/runConig/OdooConfigurationFragmentEditor.kt
+++ src/main/kotlin/com/github/lseodoo/odoorunconfig/runConig/OdooConfigurationFragmentEditor.kt
@@ -2,8 +2,10 @@ package com.github.lseodoo.odoorunconfig.runConig
 
 import com.github.lseodoo.odoorunconfig.runConfig.OdooRunConfiguration
 import com.intellij.execution.ui.SettingsEditorFragment
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.ui.LabeledComponent
-import com.intellij.ui.components.JBTextField
+import com.intellij.openapi.ui.TextBrowseFolderListener
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.jetbrains.python.run.PythonRunConfiguration
 import com.jetbrains.python.run.configuration.AbstractPythonConfigurationFragmentedEditor
 
@@ -12,21 +14,22 @@ class OdooConfigurationFragmentEditor(odooRunConfiguration: OdooRunConfiguration
     : AbstractPythonConfigurationFragmentedEditor<PythonRunConfiguration>(odooRunConfiguration) {
 
     override fun customizeFragments(fragments: MutableList<SettingsEditorFragment<PythonRunConfiguration, *>>) {
-//        super.customizeFragments(fragments) // Can't call as parent is abstrat
+        // super.customizeFragments(fragments) // Can't call as parent is abstract
         addToFragmentsBeforeEditors(fragments, createOdooCustomFieldFragment())
     }
 
-    private fun createOdooCustomFieldFragment(): SettingsEditorFragment<PythonRunConfiguration, LabeledComponent<JBTextField>> {
-        val textField = JBTextField()
-        val component = LabeledComponent.create(textField, "Odoo Custom Field:")
+    private fun createOdooCustomFieldFragment(): SettingsEditorFragment<PythonRunConfiguration, LabeledComponent<TextFieldWithBrowseButton>> {
+        val odoobinPathWidget = TextFieldWithBrowseButton()
+        odoobinPathWidget.addBrowseFolderListener(TextBrowseFolderListener(FileChooserDescriptorFactory.singleFile()))
+        val component = LabeledComponent.create(odoobinPathWidget, "Path for `odoo-bin` file")
 
         return SettingsEditorFragment(
-            "odoo.my.custom.field",
-            "Odoo Settings",
+            "odoo.script.odoo-bin.file.path",
+            "Path for `odoo-bin` file",
             "Odoo",
             component,
-            { config, _ -> (config as? OdooRunConfiguration)?.let { textField.text = it.myCustomField } },
-            { config, _ -> (config as? OdooRunConfiguration)?.let { it.myCustomField = textField.text } },
+            { config, _ -> (config as? OdooRunConfiguration)?.let { odoobinPathWidget.text = it.odooBinFilePath } },
+            { config, _ -> (config as? OdooRunConfiguration)?.let { it.odooBinFilePath = odoobinPathWidget.text } },
             { true }
         )
     }
