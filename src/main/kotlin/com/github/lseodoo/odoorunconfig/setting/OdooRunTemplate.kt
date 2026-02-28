fad09830af2c93aea5d5c9b3fe534f4916ec66bd
lse-odoo <lse@odoo.com>
2026-02-28 15:44:01 +0100

    [REM] Remove unused `ColumnInfo` subclasses from `OdooRunTemplate` file
    - Deleted `NameColumn` and `VersionColumn` classes as they are no longer used.
    
    (was used before in the table of run configuration)

--- src/main/kotlin/com/github/lseodoo/odoorunconfig/setting/OdooRunTemplate.kt
+++ src/main/kotlin/com/github/lseodoo/odoorunconfig/setting/OdooRunTemplate.kt
@@ -1,27 +1,12 @@
 package com.github.lseodoo.odoorunconfig.setting
 
 import com.github.lseodoo.odoorunconfig.common.OdooRunConfig
-import com.intellij.util.ui.ColumnInfo
 
 data class OdooRunTemplate(
     var name: String="Standard",
     var version: String="19.0",
     var runConfig: OdooRunConfig = OdooRunConfig(),
 )
-
-
-class NameColumn : ColumnInfo<OdooRunTemplate, String>("Name") {
-    override fun valueOf(item: OdooRunTemplate): String = item.name
-    override fun setValue(item: OdooRunTemplate, value: String) { item.name = value }
-    override fun isCellEditable(item: OdooRunTemplate): Boolean = true
-}
-
-
-class VersionColumn : ColumnInfo<OdooRunTemplate, String>("Version") {
-    override fun valueOf(item: OdooRunTemplate): String = item.version
-    override fun setValue(item: OdooRunTemplate, value: String) { item.version = value }
-    override fun isCellEditable(item: OdooRunTemplate): Boolean = true
-}
 
 data class OdooState(
     var runTemplates: MutableList<OdooRunTemplate> = mutableListOf()
